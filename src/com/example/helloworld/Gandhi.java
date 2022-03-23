0b347d7fbba1f2ad0ed126513eed314998d3db2e
Amol2807 <amoldgandhi@gmail.com>
2022-03-24 00:26:14 +0530

    New Branch Commit

---
+++ src/com/example/helloworld/Gandhi.java
@@ -0,0 +1,15 @@
+package com.example.helloworld;
+
+public class Gandhi {
+
+    int a;
+    Gandhi()
+    {
+        a = 5;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Amol Gandhi in a new File");
+    }
+
+}
--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -4,5 +4,9 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+        Gandhi gandhi = new Gandhi();
+        System.out.println(gandhi.a);
+        System.out.println("Hello World1");
     }
 }
+
